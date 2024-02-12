d47d212ae3a1833a43265e5a952dd08698e6f98a
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-02-11 23:27:08 -0800

    a

--- foodGrabMobile/app/_layout.tsx
+++ foodGrabMobile/app/_layout.tsx
@@ -8,7 +8,7 @@ import { useEffect } from 'react';
 import { useColorScheme } from '@/components/useColorScheme';
 
 export {
-  // Catch any errors thrown by the Layout component.
+  
   ErrorBoundary,
 } from 'expo-router';
 
