495bb93d8c3437cf862d88cb09f18bc36543cefb
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-03-12 00:10:57 -0700

    a

--- foodGrabMobile/app/(tabs)/cart.jsx
+++ foodGrabMobile/app/(tabs)/cart.jsx
@@ -1,11 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import Colors from '@/constants/Colors';
 
 const cart = () => {
+
+  const [addedItems, setAddedItems] = useState(false)
   return (
     <SafeAreaView style={styles.container}>
-      <Text>My Cart</Text>
+      <Text style={{paddingLeft : 20, fontFamily : 'Railway2', paddingTop : 30, fontSize : 20}}>My Cart</Text>
+
+      {addedItems ? (
+
+        <View style={styles.container2}>
+
+        <View >
+          <Image 
+            source={require('../../assets/images/Box.png')}
+          />
+        </View>
+          <Text style={{paddingVertical : 20, fontFamily : 'Railway1'}}>Your Cart is empty</Text>
+
+        <TouchableOpacity style={styles.btnStyles}>
+          <Text style={{color : 'white'}}>Add Items to cart</Text>
+        </TouchableOpacity>
+        </View>
+
+      ) : (
+
+        <View style={styles.container2}>
+
+        <View >
+          <Image 
+            source={require('../../assets/images/Box.png')}
+          />
+        </View>
+          <Text style={{paddingVertical : 20, fontFamily : 'Railway1'}}>Your Cart is empty</Text>
+
+        <TouchableOpacity style={styles.btnStyles}>
+          <Text style={{color : 'white'}}>Add Items to cart</Text>
+        </TouchableOpacity>
+      </View>
+      )}
     </SafeAreaView>
   )
 }
@@ -15,7 +51,29 @@ export default cart
 const styles = StyleSheet.create({
   container : {
     flex : 1,
+
+  },
+
+
+  container2 : {
+    flex : 1,
     justifyContent : 'center',
     alignItems: 'center',
-  }
+    paddingHorizontal : 20
+  },
+
+  btnStyles :{
+    height : 40,
+    backgroundColor : Colors.myRed,
+    flexDirection : 'row',
+    alignItems : 'center',
+    paddingHorizontal : 20,
+    justifyContent : 'center',
+    borderRadius : 10,  
+    marginTop : 10,
+    width : '80%',
+},
+
+
+  
 })
